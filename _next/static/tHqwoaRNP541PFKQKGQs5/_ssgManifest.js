b062888a8c63ad4a336d9e93d6dce7fe006d17a0
2myeonghee <2myeonghee@users.noreply.github.com>
2026-03-04 16:09:44 +0000

    deploy: 00324bc0c60db0291bfb9829973835984b2fea60

---
+++ _next/static/tHqwoaRNP541PFKQKGQs5/_ssgManifest.js
@@ -0,0 +1,1 @@
+self.__SSG_MANIFEST=new Set(["\u002Fadmin\u002Fapplications\u002F[id]","\u002Fadmin\u002Fdistricts\u002F[id]","\u002Fdistricts\u002F[id]"]);self.__SSG_MANIFEST_CB&&self.__SSG_MANIFEST_CB()
